cd10325979fc18232b301bc32a31969d5e583501
Robert Kuznetsov <robertkuznetsou@gmail.com>
2023-11-15 09:22:55 +0100

    Revers integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,21 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const strNumber = n.toString().split(''); 
+  const reversedInt = parseInt(strNumber.reverse().join(''))
+  console.log(reversedInt)
+  if (n < 0) {
+    const reversedIntWithMinus = reversedInt * -1;
+    console.log(n < 0, n, reversedIntWithMinus, reversedInt * -1)
+    return reversedIntWithMinus;
+  } else {
+    return reversedInt;
+  }
+}
+
+reverseInt(189)
+reverseInt(-51)
+reverseInt(51)
 
 module.exports = reverseInt;

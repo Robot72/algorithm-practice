fa7409bbaa1e7459ef632b584bc98936c9eadb21
Robert Kuznetsov <robertkuznetsou@gmail.com>
2024-02-19 21:31:38 +0300

    Draft implementation oof lined list (few methods)

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,44 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null) {
+        this.data = data
+    }
 
-class LinkedList {}
+
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    /**
+     * Not implemented 
+     * @param {*} index 
+     * @returns 
+     */
+    getAd(index) {
+        return this.node;
+    }
+
+    removeAt(index) {
+        if (!this.node) {
+            return;
+        }
+
+        if (index === 0) {
+            this.head = this.head.next;
+            return;
+        }
+
+        const previous = this.getAt(index - 1);
+        if (!previous || !previous.next) {
+            return;
+        }
+        previous.next = previous.next.next;
+    }
+}
 
 module.exports = { Node, LinkedList };
